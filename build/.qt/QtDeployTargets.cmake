bedb066885949c15b92453405d363e1d7a74a76c
notfatnotfunny <165948792+notfatnotfunny@users.noreply.github.com>
2025-07-06 10:12:54 +0200

    Delete build directory
    It doesn't make sense to have it since every device build differently

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_MyApp_FILE /home/notfatnotfunny/Documents/codeIsFun/screenlock/build/MyApp)
-set(__QT_DEPLOY_TARGET_MyApp_TYPE EXECUTABLE)
--- build/CMakeFiles/MyApp.dir/test.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MyApp.dir/test.cpp.o: \
- /home/notfatnotfunny/Documents/codeIsFun/screenlock/test.cpp \
- /usr/include/stdc-predef.h
